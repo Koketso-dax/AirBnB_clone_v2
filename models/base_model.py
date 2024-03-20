61ba313e6904901dd7501274a00386ff4f6c7f9d
Koketso-dax <lanchaster.k@gmail.com>
2024-03-20 14:25:39 +0200

    Updated pep8 standard

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,7 @@ if models.storage_t == "db":
 else:
     Base = object
 
+
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if getenv("HBNB_TYPE_STORAGE") == 'db':
@@ -43,7 +44,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = self.created_at
-
 
     def __str__(self):
         """String representation of the BaseModel class"""
