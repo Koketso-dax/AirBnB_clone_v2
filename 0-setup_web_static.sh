058c1507cef24244b7a115fc9b16937a2c1a66f9
Koketso-dax <lanchaster.k@gmail.com>
2024-04-03 22:31:03 +0200

    Final touch

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
 # Install Nginx if not already installed
 sudo apt-get update
 sudo apt-get install nginx -y
-
+sudo ufw allow 'Nginx HTTP'
 # Create necessary directories
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
@@ -13,6 +12,7 @@ sudo mkdir -p /data/web_static/releases/test/
 
 # Create a fake HTML file
 sudo touch /data/web_static/releases/test/index.html
+
 sudo bash -c 'cat > /data/web_static/releases/test/index.html' << EOF
 <html>
 <head></head>
@@ -21,7 +21,7 @@ sudo bash -c 'cat > /data/web_static/releases/test/index.html' << EOF
 EOF
 
 # Create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 # Set ownership recursively to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
@@ -29,11 +29,34 @@ sudo chown -R ubuntu:ubuntu /data/
 # Remove existing symbolic link if it exists
 sudo rm -f /etc/nginx/sites-enabled/default
 
+# Configure a default setup
+sudo bash -c 'cat > /etc/nginx/sites-available/default' << EOF
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    add_header X-Served-By \$hostname;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}
+EOF
+
 # Create symbolic link to Nginx configuration file
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 
 # Add location block to Nginx configuration file
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/; }' "/etc/nginx/sites-enabled/default"
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
 
 # Restart Nginx
 sudo service nginx restart
