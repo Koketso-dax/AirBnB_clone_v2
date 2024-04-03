ae32cc797e91ccca2cbb75675cb0150051a21ee6
Koketso-dax <lanchaster.k@gmail.com>
2024-04-03 22:17:25 +0200

    I dont know whats wrong

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Install Nginx if not already installed
 sudo apt-get update
 sudo apt-get install nginx -y
@@ -25,29 +26,13 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Set ownership recursively to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo bash -c 'cat > /etc/nginx/sites-available/default' << EOF
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+# Remove existing symbolic link if it exists
+sudo rm -f /etc/nginx/sites-enabled/default
 
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    add_header X-Served-By \$hostname;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-}
-EOF
-
+# Create symbolic link to Nginx configuration file
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+
+# Add location block to Nginx configuration file
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/; }' "/etc/nginx/sites-enabled/default"
 
 # Restart Nginx
