54bfc3e58e0dbd85c2d174684c8635e254b41d6b
Koketso-dax <lanchaster.k@gmail.com>
2024-04-22 21:15:24 +0200

    Test 7-list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,24 +2,17 @@
 """
 starts a Flask web application
 """
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from flask import Flask, render_template
-from models import storage, state
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    display a HTML page with the states listed in alphabetical order
-    """
-    if isinstance(storage, storage.DBStorage):
-        states = storage.all(state).values()
-    else:
-        states = sorted(list(storage.all("State")
-                             .values()), key=lambda x: x.name)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
@@ -27,7 +20,6 @@ def states_list():
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
