ef6b7b293f636d96212263d980e96c8299935c9a
Koketso-dax <lanchaster.k@gmail.com>
2024-04-22 20:19:43 +0200

    Included dynamic root dir import

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,10 @@
 starts a Flask web application
 """
 from flask import Flask, render_template
+import os
+import sys
+# Add parent directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from models import *
 from models import storage
 app = Flask(__name__)
