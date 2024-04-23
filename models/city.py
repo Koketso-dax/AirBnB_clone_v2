925b408163d958b950ef69554d2a27c8b41ee834
Koketso-dax <lanchaster.k@gmail.com>
2024-04-23 22:11:22 +0200

    Update state

--- models/city.py
+++ models/city.py
@@ -4,8 +4,6 @@ City Class Module
 """
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
