5e07309a449b2325d1f591938b9eb131664f9124
Koketso-dax <lanchaster.k@gmail.com>
2024-04-23 13:34:59 +0200

    I dont know anymore

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,10 +8,12 @@ from models.state import State
 from models import storage
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
